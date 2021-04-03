24503bca68f8f9a4cf7382c1a5c476a4fbbad341
Oleh Kinchevskyi <Oleh.Kinchevskyi@gmail.com>
2021-04-03 11:09:12 +0300

    Додає ДЗ 8

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import './Spinner.scss';
+
+const Spinner = () => {
+    return (
+        <div className="Loader">
+             <Loader
+                 type="ThreeDots"
+                 color="#00BFFF"
+                 height={100}
+                 width={100}
+                timeout={300} 
+                />
+        </div>
+    );
+};
+
+export default Spinner;
+
---
+++ src/redux/store.js
@@ -0,0 +1,40 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'; 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import phoneBookReducer from './phoneBook/phoneBook-reducer';
+import authReducer from './auth/auth-reducer';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token']
+};
+
+const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    phoneBook: phoneBookReducer,
+    },
+    middleware,
+    devTools: process.env.NODE_ENV === 'development',
+})
+
+const persistor = persistStore(store);
+export default { store, persistor };
